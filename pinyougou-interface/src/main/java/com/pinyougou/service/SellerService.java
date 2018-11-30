d5150c760c2b4efaa0eceb4b8aa560729619c592
jindaye <jindaye@qq.com>
2018-11-30 16:30:09 +0800

    模块三sellerController.js修改

--- pinyougou-interface/src/main/java/com/pinyougou/service/SellerService.java
+++ pinyougou-interface/src/main/java/com/pinyougou/service/SellerService.java
@@ -6,52 +6,9 @@ import java.util.List;
 import java.io.Serializable;
 /**
  * SellerService 服务接口
- * @date 2018-10-31 15:53:42
+ * @date 2018-10-30 16:10:23
  * @version 1.0
  */
-/*
-public interface SellerService {
-
-	*/
-/** 添加方法 *//*
-
-	void save(Seller seller);
-
-	*/
-/** 修改方法 *//*
-
-	void update(Seller seller);
-
-	*/
-/** 根据主键id删除 *//*
-
-	void delete(Serializable id);
-
-	*/
-/** 批量删除 *//*
-
-	void deleteAll(Serializable[] ids);
-
-	*/
-/** 根据主键id查询 *//*
-
-	Seller findOne(Serializable id);
-
-	*/
-/** 查询全部 *//*
-
-	List<Seller> findAll();
-
-	*/
-/** 多条件分页查询 *//*
-
-	PageResult findByPage(Seller seller, int page, int rows);
-
-	*/
-/** 修改商家状态 *//*
-
-	void updateStatus(String sellerId, String status);
-}*/
 public interface SellerService {
 
 	/** 添加方法 */
