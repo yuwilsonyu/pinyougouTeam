c333a253bbe5a1be698567460065ab88eac7be07
jindaye <jindaye@qq.com>
2018-11-30 11:39:15 +0800

    模块三sellerservice接口

--- pinyougou-interface/src/main/java/com/pinyougou/service/SellerService.java
+++ pinyougou-interface/src/main/java/com/pinyougou/service/SellerService.java
@@ -9,6 +9,49 @@ import java.io.Serializable;
  * @date 2018-10-31 15:53:42
  * @version 1.0
  */
+/*
+public interface SellerService {
+
+	*/
+/** 添加方法 *//*
+
+	void save(Seller seller);
+
+	*/
+/** 修改方法 *//*
+
+	void update(Seller seller);
+
+	*/
+/** 根据主键id删除 *//*
+
+	void delete(Serializable id);
+
+	*/
+/** 批量删除 *//*
+
+	void deleteAll(Serializable[] ids);
+
+	*/
+/** 根据主键id查询 *//*
+
+	Seller findOne(Serializable id);
+
+	*/
+/** 查询全部 *//*
+
+	List<Seller> findAll();
+
+	*/
+/** 多条件分页查询 *//*
+
+	PageResult findByPage(Seller seller, int page, int rows);
+
+	*/
+/** 修改商家状态 *//*
+
+	void updateStatus(String sellerId, String status);
+}*/
 public interface SellerService {
 
 	/** 添加方法 */
@@ -32,6 +75,7 @@ public interface SellerService {
 	/** 多条件分页查询 */
 	PageResult findByPage(Seller seller, int page, int rows);
 
-	/** 修改商家状态 */
-	void updateStatus(String sellerId, String status);
+	void updatePassword(String newPassword, String sellerId);
+
+	String findPassword(String sellerId);
 }
