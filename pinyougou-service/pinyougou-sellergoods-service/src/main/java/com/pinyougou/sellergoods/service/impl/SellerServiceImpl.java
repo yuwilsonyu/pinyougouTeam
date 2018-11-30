c7d4fc2d04ec3571d463a6d50647384bcd596a97
jindaye <jindaye@qq.com>
2018-11-30 16:29:18 +0800

    模块三seller-goods-service修改

--- pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SellerServiceImpl.java
+++ pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SellerServiceImpl.java
@@ -19,116 +19,11 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * 商家服务接口实现类
+ * SellerServiceImpl 服务接口实现类
  *
- * @author lee.siu.wah
  * @version 1.0
- * <p>File Created at 2018-11-05<p>
+ * @date 2018-10-30 16:10:23
  */
-/*
-@Service(interfaceName = "com.pinyougou.service.SellerService")
-@Transactional
-public class SellerServiceImpl implements SellerService {
-
-    @Autowired
-    private SellerMapper sellerMapper;
-
-    */
-/** 商家申请入驻 *//*
-
-    @Override
-    public void save(Seller seller) {
-        try{
-            // 商家审核状态(未审核)
-            seller.setStatus("0");
-            // 创建时间
-            seller.setCreateTime(new Date());
-            sellerMapper.insertSelective(seller);
-        }catch (Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-
-    @Override
-    public void update(Seller seller) {
-
-    }
-
-    @Override
-    public void delete(Serializable id) {
-
-    }
-
-    @Override
-    public void deleteAll(Serializable[] ids) {
-
-    }
-
-    @Override
-    public Seller findOne(Serializable id) {
-        try{
-            return sellerMapper.selectByPrimaryKey(id);
-        }catch (Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-
-    @Override
-    public List<Seller> findAll() {
-        return null;
-    }
-
-    @Override
-    public PageResult findByPage(Seller seller, int page, int rows) {
-        try{
-            // 开始分页
-            PageInfo<Seller> pageInfo = PageHelper.startPage(page, rows)
-                    .doSelectPageInfo(new ISelect() {
-                @Override
-                public void doSelect() {
-                    // 创建示范对象
-                    Example example = new Example(Seller.class);
-                    // 创建条件对象
-                    Example.Criteria criteria = example.createCriteria();
-                    // 状态码 status = ?
-                    if (seller != null && !StringUtils.isEmpty(seller.getStatus())) {
-                        criteria.andEqualTo("status", seller.getStatus());
-                    }
-                    // 公司名称 name like ?
-                    if (seller != null && !StringUtils.isEmpty(seller.getName())){
-                        criteria.andLike("name", "%"+ seller.getName() +"%");
-                    }
-                    // 店铺名称 nick_name like ?
-                    if (seller != null && !StringUtils.isEmpty(seller.getNickName())){
-                        criteria.andLike("nickName", "%"+ seller.getNickName() +"%");
-                    }
-                    sellerMapper.selectByExample(example);
-                }
-            });
-            return new PageResult(pageInfo.getTotal(), pageInfo.getList());
-        }catch (Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-
-    */
-/** 修改商家状态 *//*
-
-    public void updateStatus(String sellerId, String status){
-        try{
-            // update tb_seller set status = ? where seller_id = ?
-            Seller seller = new Seller();
-            seller.setSellerId(sellerId);
-            seller.setStatus(status);
-            sellerMapper.updateByPrimaryKeySelective(seller);
-        }catch (Exception ex){
-            throw new RuntimeException(ex);
-        }
-    }
-}
-*/
-
-
 @Service(interfaceName = "com.pinyougou.service.SellerService")
 @Transactional
 public class SellerServiceImpl implements SellerService {
