bf468821ea408ef6400520978b367dc6c2785a09
jindaye <jindaye@qq.com>
2018-11-30 11:38:21 +0800

    模块三sellerserviceImpl

--- pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SellerServiceImpl.java
+++ pinyougou-service/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/SellerServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -24,6 +25,7 @@ import java.util.List;
  * @version 1.0
  * <p>File Created at 2018-11-05<p>
  */
+/*
 @Service(interfaceName = "com.pinyougou.service.SellerService")
 @Transactional
 public class SellerServiceImpl implements SellerService {
@@ -31,7 +33,9 @@ public class SellerServiceImpl implements SellerService {
     @Autowired
     private SellerMapper sellerMapper;
 
-    /** 商家申请入驻 */
+    */
+/** 商家申请入驻 *//*
+
     @Override
     public void save(Seller seller) {
         try{
@@ -107,7 +111,9 @@ public class SellerServiceImpl implements SellerService {
         }
     }
 
-    /** 修改商家状态 */
+    */
+/** 修改商家状态 *//*
+
     public void updateStatus(String sellerId, String status){
         try{
             // update tb_seller set status = ? where seller_id = ?
@@ -119,4 +125,137 @@ public class SellerServiceImpl implements SellerService {
             throw new RuntimeException(ex);
         }
     }
+}
+*/
+
+
+@Service(interfaceName = "com.pinyougou.service.SellerService")
+@Transactional
+public class SellerServiceImpl implements SellerService {
+
+    @Autowired
+    private SellerMapper sellerMapper;
+
+
+    /** 从数据库查询用户旧密码 */
+    public String findPassword(String sellerId) {
+        return sellerMapper.findPassword(sellerId);
+    }
+    /** 修改密码 */
+    public void updatePassword(String newPassword, String sellerId) {
+        try {
+            sellerMapper.updatePassword(newPassword, sellerId);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * 添加方法
+     */
+    public void save(Seller seller) {
+        try {
+            seller.setCreateTime(new Date());
+            seller.setStatus("0");
+            sellerMapper.insertSelective(seller);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * 修改方法
+     */
+    public void update(Seller seller) {
+        try {
+            sellerMapper.updateByPrimaryKeySelective(seller);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * 根据主键id删除
+     */
+    public void delete(Serializable id) {
+        try {
+            sellerMapper.deleteByPrimaryKey(id);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * 批量删除
+     */
+    public void deleteAll(Serializable[] ids) {
+        try {
+            // 创建示范对象
+            Example example = new Example(Seller.class);
+            // 创建条件对象
+            Example.Criteria criteria = example.createCriteria();
+            // 创建In条件
+            criteria.andIn("id", Arrays.asList(ids));
+            // 根据示范对象删除
+            sellerMapper.deleteByExample(example);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * 根据主键id查询
+     */
+    public Seller findOne(Serializable id) {
+        try {
+            return sellerMapper.selectByPrimaryKey(id);
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * 查询全部
+     */
+    public List<Seller> findAll() {
+        try {
+            return sellerMapper.selectAll();
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * 多条件分页查询
+     */
+    public PageResult findByPage(Seller seller, int page, int rows) {
+        try {
+            PageInfo<Seller> pageInfo = PageHelper.startPage(page, rows)
+                    .doSelectPageInfo(new ISelect() {
+                        @Override
+                        public void doSelect() {
+                            // 创建示范对象
+                            Example example = new Example(Seller.class);
+                            // 创建条件对象
+                            Example.Criteria criteria = example.createCriteria();
+                            if (seller != null && !StringUtils.isEmpty(seller.getStatus())) {
+                                criteria.andEqualTo("status", seller.getStatus());
+                            }
+                            if (seller != null && !StringUtils.isEmpty(seller.getName())) {
+                                criteria.andLike("name", seller.getName());
+                            }
+                            if (seller != null && !StringUtils.isEmpty(seller.getNickName())) {
+                                criteria.andLike("nickName", seller.getNickName());
+                            }
+                            sellerMapper.selectByExample(example);
+                        }
+                    });
+            return new PageResult(pageInfo.getTotal(), pageInfo.getList());
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+
 }
